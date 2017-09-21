7715242dab5eb660aab265fdcce9145acd90cb8e
QC87 <QC87@INLTD12361.global.tesco.org>
2017-09-21 23:50:09 +0530

    Notification Service

---
+++ mobile/src/main/java/com/tesco/services/poc/stocknotification/activity/ManageProductMappingActivity.java
@@ -0,0 +1,11 @@
+package com.tesco.services.poc.stocknotification.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by QC87 on 9/21/2017.
+ */
+public class ManageProductMappingActivity  extends Activity {
+
+
+}
---
+++ mobile/src/main/java/com/tesco/services/poc/stocknotification/app/Config.java
@@ -0,0 +1,24 @@
+package com.tesco.services.poc.stocknotification.app;
+
+/**
+ * Created by QC87 on 9/21/2017.
+ */
+public class Config {
+
+
+
+        // global topic to receive app wide push notifications
+        public static final String TOPIC_GLOBAL = "global";
+
+        // broadcast receiver intent filters
+        public static final String REGISTRATION_COMPLETE = "registrationComplete";
+        public static final String PUSH_NOTIFICATION = "pushNotification";
+
+        // id to handle the notification in the notification tray
+        public static final int NOTIFICATION_ID = 100;
+        public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
+        public static final String SHARED_PREF = "ah_firebase";
+
+
+}
